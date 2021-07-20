e619ef5ba4d37aaa20e396475313bcce13b9e58b
Reallocman <61756155+Reallocman@users.noreply.github.com>
2021-07-20 15:58:05 +0300

    Delete Task_3 directory

--- Task_3/average/average.go
+++
@@ -1,9 +0,0 @@
-package average
-
-func Mean(array [6]float64 )  float64 {
-	var sum float64
-	for i:=0;i<len(array);i++{
-		sum+=array[i]
-	}
-	return sum/float64(len(array))
-}
--- Task_3/go.mod
+++
@@ -1,3 +0,0 @@
-module storages
-
-go 1.16
