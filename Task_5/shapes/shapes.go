e46fcb6fd59eb90dc3c36ccb91a7dcf497cec79a
Reallocman <61756155+Reallocman@users.noreply.github.com>
2021-07-23 14:12:58 +0300

    Update shapes.go

--- Task_5/shapes/shapes.go
+++ Task_5/shapes/shapes.go
@@ -1,8 +1,6 @@
 package shapes
 
-import (
-	"fmt"
-)
+import "fmt"
 
 type Shape interface {
 	Area() (float64,error)
@@ -11,16 +9,15 @@ type Shape interface {
 
 func DescribeShape(s Shape) {
 	area,err:=s.Area()
-	if err!=nil{
+	if err!=nil {
 		fmt.Println(err)
 		return
 	}
 	perimetr,err:=s.Perimeter()
-	if err!=nil{
+	if err!=nil {
 		fmt.Println(err)
 		return
 	}
-
 	fmt.Println(s)
 	fmt.Printf("Area: %.2f\n",area)
 	fmt.Printf("Perimeter: %.2f\n",perimetr)
